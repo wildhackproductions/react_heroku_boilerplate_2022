2e746741170382d7d4e13ae3a58164ea8d0c17d4
Wild Hack <wildhackproductions@gmail.com>
2021-10-30 11:45:45 +1100

    First local commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Landing from './component/Landing'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Landing />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/component/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <div>
+      <h1 style={{color: "#38b6ff"}}>React Heroku Boiler 2022!</h1>
+    </div>
+  )
+}
+
+export default Landing
